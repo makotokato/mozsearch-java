38074be93e6a35744210f55f8811148fcc949362
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2019-03-15 15:25:04 +0900

    reformat using Google Java Style

--- src/main/java/net/wontfix/mozsearch/JavaAnalyzer.java
+++ src/main/java/net/wontfix/mozsearch/JavaAnalyzer.java
@@ -1,49 +1,48 @@
 package net.wontfix.mozsearch;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.File;
-import java.io.IOException;
-import java.util.Optional;
-
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.*;
-import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 public class JavaAnalyzer {
-    public static void main(String[] args) {
-        Path path = Paths.get(args[0]);
-        try {
-            CompilationUnit unit = StaticJavaParser.parse(path);
-            String packagename = "";
-            Optional<PackageDeclaration> p =  unit.getPackageDeclaration();
-            if (p.isPresent()) {
-                packagename = p.get().getName().toString();
-            }
+  public static void main(String[] args) {
+    Path path = Paths.get(args[0]);
+    try {
+      CompilationUnit unit = StaticJavaParser.parse(path);
+      String packagename = "";
+      Optional<PackageDeclaration> p = unit.getPackageDeclaration();
+      if (p.isPresent()) {
+        packagename = p.get().getName().toString();
+      }
 
-            CombinedTypeSolver solver = new CombinedTypeSolver();
-            solver.add(new ReflectionTypeSolver());
-            solver.add(new JavaParserTypeSolver(new File(path.getParent().toString())));
-            if (packagename.length() > 0) {
-                String t = packagename;
-                Path directory = path.getParent();
-                int pos = 0;
-                while (pos >= 0) {
-                    directory = directory.getParent();
-                    solver.add(new JavaParserTypeSolver(new File(directory.toString())));
-                    pos = t.indexOf('.');
-                    t = t.substring(pos + 1);
-                }
-            }
-    
-            unit.accept(new MozSearchVisitor(solver), packagename + ".");
-        } catch (IOException e) {
-            System.err.println(e);
+      CombinedTypeSolver solver = new CombinedTypeSolver();
+      solver.add(new ReflectionTypeSolver());
+      solver.add(new JavaParserTypeSolver(new File(path.getParent().toString())));
+      if (packagename.length() > 0) {
+        String t = packagename;
+        Path directory = path.getParent();
+        int pos = 0;
+        while (pos >= 0) {
+          directory = directory.getParent();
+          solver.add(new JavaParserTypeSolver(new File(directory.toString())));
+          pos = t.indexOf('.');
+          t = t.substring(pos + 1);
         }
+      }
+
+      unit.accept(new MozSearchVisitor(solver), packagename + ".");
+    } catch (IOException e) {
+      System.err.println(e);
     }
+  }
 }
--- src/main/java/net/wontfix/mozsearch/MozSearchVisitor.java
+++ src/main/java/net/wontfix/mozsearch/MozSearchVisitor.java
@@ -3,173 +3,172 @@ package net.wontfix.mozsearch;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.SimpleName;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.ast.type.Type;
-import com.github.javaparser.ast.type.TypeParameter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
-import com.github.javaparser.resolution.types.ResolvedType;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
-
 import java.util.Optional;
 
 public class MozSearchVisitor extends VoidVisitorAdapter<String> {
-    private CombinedTypeSolver mSolver;
+  private CombinedTypeSolver mSolver;
 
-    public MozSearchVisitor(CombinedTypeSolver aSolver) {
-        mSolver = aSolver;
+  public MozSearchVisitor(CombinedTypeSolver aSolver) {
+    mSolver = aSolver;
+  }
+
+  private static void outputSource(final Node n, final SimpleName name) {
+    outputSource(n, name, "");
+  }
+
+  private static void outputSource(final Node n, final SimpleName name, final String scope) {
+    System.out.print("{\"loc\":\"");
+    System.out.print(
+        name.getBegin().get().line
+            + ":"
+            + name.getBegin().get().column
+            + "-"
+            + (name.getBegin().get().column + name.getIdentifier().length()));
+    System.out.print("\",");
+    System.out.print("\"source\":1,");
+    if (n instanceof ClassOrInterfaceDeclaration) {
+      System.out.print("\"syntax\":\"def,prop\",");
+      System.out.print("\"pretty\":\"type " + scope + name.getIdentifier() + "\",");
+    } else if (n instanceof ClassOrInterfaceType) {
+      System.out.print("\"syntax\":\"use,prop\",");
+      System.out.print("\"pretty\":\"type " + name.getIdentifier() + "\",");
+    } else if (n instanceof VariableDeclarator) {
+      System.out.print("\"syntax\":\"def,prop\",");
+      System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
+    } else if (n instanceof Parameter) {
+      System.out.print("\"syntax\":\"def,prop\",");
+      System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
+    } else if (n instanceof MethodDeclaration) {
+      System.out.print("\"syntax\":\"decl\",");
+      System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
+    } else if (n instanceof MethodCallExpr) {
+      System.out.print("\"syntax\":\"use,prop\",");
+      System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
+    } else if (n instanceof NameExpr) {
+      System.out.print("\"syntax\":\"use,prop\",");
+      System.out.print("\"pretty\":\"property " + name.getIdentifier() + "\",");
+    }
+    System.out.print("\"sym\":\"" + scope + name.getIdentifier() + "\"");
+    System.out.println("},");
+  }
+
+  private void outputTarget(final Node n, final SimpleName name) {
+    outputTarget(n, name, "");
+  }
+
+  private void outputTarget(final Node n, final SimpleName name, final String scope) {
+    System.out.print("{\"loc\":\"");
+    System.out.print(name.getBegin().get().line + ":" + name.getBegin().get().column);
+    System.out.print("\",");
+    System.out.print("\"target\":1,");
+
+    if (n instanceof ClassOrInterfaceDeclaration) {
+      System.out.print("\"kind\":\"decl\",");
+      System.out.print("\"pretty\":\"type " + scope + name.getIdentifier() + "\",");
+    } else if (n instanceof ClassOrInterfaceType) {
+      System.out.print("\"kind\":\"use\",");
+      System.out.print("\"pretty\":\"type " + name.getIdentifier() + "\",");
+    } else if (n instanceof Parameter) {
+      System.out.print("\"kind\":\"def\",");
+      System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
+    } else if (n instanceof VariableDeclarator) {
+      System.out.print("\"kind\":\"def\",");
+      System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
+    } else if (n instanceof MethodDeclaration) {
+      System.out.print("\"kind\":\"decl\",");
+      System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
+    } else if (n instanceof MethodCallExpr) {
+      System.out.print("\"kind\":\"use\",");
+      System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
+    } else if (n instanceof NameExpr) {
+      System.out.print("\"kind\":\"use\",");
+      System.out.print("\"pretty\":\"property " + name.getIdentifier() + "\",");
     }
 
-    private static void outputSource(final Node n, final SimpleName name) {
-        outputSource(n, name, "");
+    System.out.print("\"sym\":\"" + scope + name.getIdentifier() + "\"");
+    System.out.println("},");
+  }
+
+  @Override
+  public void visit(ClassOrInterfaceDeclaration n, String a) {
+    outputSource(n, n.getName(), a);
+    outputTarget(n, n.getName(), a);
+
+    for (ClassOrInterfaceType classType : n.getExtendedTypes()) {
+      outputSource(classType, classType.getName());
+      outputTarget(classType, classType.getName());
     }
-    private static void outputSource(final Node n, final SimpleName name, final String scope) {
-        System.out.print("{\"loc\":\"");
-        System.out.print(name.getBegin().get().line + ":" + name.getBegin().get().column + "-" + (name.getBegin().get().column + name.getIdentifier().length()));
-        System.out.print("\",");
-        System.out.print("\"source\":1,");
-        if (n instanceof ClassOrInterfaceDeclaration) {
-            System.out.print("\"syntax\":\"def,prop\",");
-            System.out.print("\"pretty\":\"type " + scope + name.getIdentifier() + "\",");
-        } else if (n instanceof ClassOrInterfaceType) {
-            System.out.print("\"syntax\":\"use,prop\",");
-            System.out.print("\"pretty\":\"type " + name.getIdentifier() + "\",");
-        } else if (n instanceof VariableDeclarator) {
-            System.out.print("\"syntax\":\"def,prop\",");
-            System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
-        } else if (n instanceof Parameter) {
-            System.out.print("\"syntax\":\"def,prop\",");
-            System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
-        } else if (n instanceof MethodDeclaration) {
-            System.out.print("\"syntax\":\"decl\",");
-            System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
-        } else if (n instanceof MethodCallExpr) {
-            System.out.print("\"syntax\":\"use,prop\",");
-            System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
-        } else if (n instanceof NameExpr) {
-            System.out.print("\"syntax\":\"use,prop\",");
-            System.out.print("\"pretty\":\"property " + name.getIdentifier() + "\",");
-        }
-        System.out.print("\"sym\":\"" + scope + name.getIdentifier() + "\"");
-        System.out.println("},");
+    for (ClassOrInterfaceType classType : n.getImplementedTypes()) {
+      outputSource(classType, classType.getName());
+      outputTarget(classType, classType.getName());
     }
+    super.visit(n, a);
+  }
 
-    private void outputTarget(final Node n, final SimpleName name) {
-        outputTarget(n, name, "");
+  @Override
+  public void visit(VariableDeclarator n, String a) {
+    String scope = "";
+    Optional<Node> parent = n.getParentNode();
+    try {
+      parent = parent.get().getParentNode();
+      if (parent.get() instanceof ClassOrInterfaceDeclaration) {
+        scope = a + ((ClassOrInterfaceDeclaration) parent.get()).getName().getIdentifier() + ".";
+      }
+    } catch (Exception e) {
     }
-    private void outputTarget(final Node n, final SimpleName name, final String scope) {
-        System.out.print("{\"loc\":\"");
-        System.out.print(name.getBegin().get().line + ":" + name.getBegin().get().column);
-        System.out.print("\",");
-        System.out.print("\"target\":1,");
+    outputSource(n, n.getName(), scope);
+    outputTarget(n, n.getName(), scope);
+    super.visit(n, a);
+  }
 
-        if (n instanceof ClassOrInterfaceDeclaration) {
-            System.out.print("\"kind\":\"decl\",");
-            System.out.print("\"pretty\":\"type " + scope + name.getIdentifier() + "\",");
-        } else if (n instanceof ClassOrInterfaceType) {
-            System.out.print("\"kind\":\"use\",");
-            System.out.print("\"pretty\":\"type " + name.getIdentifier() + "\",");
-        } else if (n instanceof Parameter) {
-            System.out.print("\"kind\":\"def\",");
-            System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
-        } else if (n instanceof VariableDeclarator) {
-            System.out.print("\"kind\":\"def\",");
-            System.out.print("\"pretty\":\"local " + name.getIdentifier() + "\",");
-        } else if (n instanceof MethodDeclaration) {
-            System.out.print("\"kind\":\"decl\",");
-            System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
-        } else if (n instanceof MethodCallExpr) {
-            System.out.print("\"kind\":\"use\",");
-            System.out.print("\"pretty\":\"function " + name.getIdentifier() + "\",");
-        } else if (n instanceof NameExpr) {
-            System.out.print("\"kind\":\"use\",");
-            System.out.print("\"pretty\":\"property " + name.getIdentifier() + "\",");
-        }
-
-        System.out.print("\"sym\":\"" + scope + name.getIdentifier() + "\"");
-        System.out.println("},");
+  @Override
+  public void visit(MethodDeclaration n, String a) {
+    String scope = "";
+    try {
+      Optional<Node> parent = n.getParentNode();
+      if (parent.get() instanceof ClassOrInterfaceDeclaration) {
+        scope = a + ((ClassOrInterfaceDeclaration) parent.get()).getName().getIdentifier() + ".";
+      }
+    } catch (Exception e) {
     }
-
-    @Override
-    public void visit(ClassOrInterfaceDeclaration n, String a) {
-       outputSource(n, n.getName(), a);
-       outputTarget(n, n.getName(), a);
-
-       for (ClassOrInterfaceType classType : n.getExtendedTypes()) {
-         outputSource(classType, classType.getName());
-         outputTarget(classType, classType.getName());
-       }
-       for (ClassOrInterfaceType classType : n.getImplementedTypes()) {
-         outputSource(classType, classType.getName());
-         outputTarget(classType, classType.getName());
-       }
-       super.visit(n, a);
+    outputSource(n, n.getName(), scope);
+    outputTarget(n, n.getName(), scope);
+    for (Parameter param : n.getParameters()) {
+      // outputSource(param, param.getName());
+      // outputTarget(param, param.getName());
     }
+    super.visit(n, a);
+  }
 
-    @Override
-    public void visit(VariableDeclarator n, String a) {
-       String scope = "";
-       Optional<Node> parent = n.getParentNode();
-       try {
-           parent = parent.get().getParentNode();
-           if (parent.get() instanceof ClassOrInterfaceDeclaration) {
-                scope = a + ((ClassOrInterfaceDeclaration)parent.get()).getName().getIdentifier() + ".";
-           }
-       } catch (Exception e) {
-       }
-       outputSource(n, n.getName(), scope);
-       outputTarget(n, n.getName(), scope);
-       super.visit(n, a);
+  @Override
+  public void visit(MethodCallExpr n, String a) {
+    String scope = "";
+    try {
+      ResolvedMethodDeclaration decl =
+          JavaParserFacade.get(mSolver).solve(n).getCorrespondingDeclaration();
+      scope = decl.getPackageName() + "." + decl.getClassName() + ".";
+    } catch (Exception e) {
     }
+    outputSource(n, n.getName(), scope);
+    outputTarget(n, n.getName(), scope);
 
-    @Override
-    public void visit(MethodDeclaration n, String a) {
-       String scope = "";
-       try {
-           Optional<Node> parent = n.getParentNode();
-           if (parent.get() instanceof ClassOrInterfaceDeclaration) {
-               scope = a + ((ClassOrInterfaceDeclaration)parent.get()).getName().getIdentifier() + ".";
-           }
-       } catch (Exception e) {
-       }
-       outputSource(n, n.getName(), scope);
-       outputTarget(n, n.getName(), scope);
-       for (Parameter param : n.getParameters()) {
-          //outputSource(param, param.getName());
-          //outputTarget(param, param.getName());
-       }
-       super.visit(n, a);
-    }
+    super.visit(n, a);
+  }
 
-    @Override
-    public void visit(MethodCallExpr n, String a) {
-       String scope = "";
-       try {
-           ResolvedMethodDeclaration decl = JavaParserFacade.get(mSolver).solve(n).getCorrespondingDeclaration();
-           scope = decl.getPackageName() + "." + decl.getClassName() + ".";
-       } catch (Exception e) {
-       }
-       outputSource(n, n.getName(), scope);
-       outputTarget(n, n.getName(), scope);
-
-       super.visit(n, a);
-    }
-
-    @Override
-    public void visit(NameExpr n, String a) {
-       //outputSource(n, n.getName());
-       //outputTarget(n, n.getName());
-       super.visit(n, a);
-    }
+  @Override
+  public void visit(NameExpr n, String a) {
+    // outputSource(n, n.getName());
+    // outputTarget(n, n.getName());
+    super.visit(n, a);
+  }
 }
